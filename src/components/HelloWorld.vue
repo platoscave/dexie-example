a98ea3715c02270d010182891cd27866f73cd7dd
platoscave <platoscave60@gmail.com>
2020-12-29 00:28:47 +0100

    initial

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,67 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <input type="text" v-model="value" />
+    <button type="button" @click="addFriend">Add Cat</button>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li class="title">LOADED FROM iDB</li>
+      <li :key="cat.id" v-for="cat in friends">
+        {{ cat.name }}
+        <button type="button" @click="remove(test)">&times;</button>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import { liveQuery } from "dexie";
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      value: "",
+      friends: [],
+    };
+  },
+  mounted() {
+    this.observeFriends();
+  },
+  methods: {
+    observeFriends() {
+      const friendsObservable = liveQuery(
+        () => this.$db.friends.toArray()
+      );
+
+      friendsObservable.subscribe({
+        start: (result) => console.log("Got start:", result),
+        next: (result) => console.log("Got result:", result),
+        complete: (result) => console.log("Got complete:", result),
+        error: (error) => console.error(error),
+      });
+
+      setTimeout(() => this.$db.friends.add({ name: "Joe", age: 78 }), 5000);
+    },
+    update() {
+      this.$db.eoscommons.toArray().then((friends) => {
+        this.friends = friends;
+        console.log(friends);
+      });
+    },
+    addFriend() {
+      this.$db.friends.add({ id: 100, name: "one", age: 2 });
+    },
+    remove(test) {
+      this.$db.eoscommons
+        .where("id")
+        .eq(test.id)
+        .delete()
+        .then(() => this.update());
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueIdb from 'vue-idb'
 
 Vue.config.productionTip = false
 
+Vue.use(VueIdb)
+
+const idb = new VueIdb({
+  version: 1,
+  database: 'eoscommons',
+  schemas: [
+    { friends: '++id, name, age' },
+    //{ preferences: 'id, property' }
+  ]
+})
+
 new Vue({
+  idb: idb,
   render: h => h(App),
 }).$mount('#app')
