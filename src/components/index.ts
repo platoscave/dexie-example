f05d8727cf669479d1845a5ea9faa1d5b0328485
platoscave <platoscave60@gmail.com>
2024-01-04 23:51:03 +0100

    fixing style

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,10 @@
+import JsonschemaTable from "./JsonschemaTable.vue";
+import JsonschemaForm from "./JsonschemaForm.vue";
+import StringCodeEditorCtrl from "./controls/StringCodeEditorCtrl.vue";
+import StringMarkdownCtrl from "./controls/StringMarkdownCtrl.vue";
+
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
-//import './style.css'
-export { default as JsonschemaForm } from "./JsonschemaForm.vue";
+import './../style.css'
+// Accommodate import, as opposed to use globally
+export { JsonschemaForm, JsonschemaTable, StringCodeEditorCtrl, StringMarkdownCtrl };
